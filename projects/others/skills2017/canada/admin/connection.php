692d5707f24ebecd5c9965a0747b6ff1b1e41d01
Harry Zhang <zh.harry@yahoo.ca>
2017-08-13 18:37:29 -0400

    Fixed Admin panel

--- projects/others/skills2017/canada/admin/connection.php
+++ projects/others/skills2017/canada/admin/connection.php
@@ -1,6 +1,8 @@
 <?php
 
-	$conn = mysqli_connect("localhost", "root", "", "wrm");
+	require_once('/etc/mysql-creds/mysql-creds.php');
+
+	$conn = mysqli_connect($mysql_creds["host"], $mysql_creds["user"], $mysql_creds["pass"], "skills2017_canada");
 	if (mysqli_connect_errno()) {
 		echo "Failed to connect to the database: " . mysqli_connect_error();
 		die();
