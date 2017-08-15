6e0e4ec97c7cca707523e01df3ede2d58f2d3907
Harry Zhang <zh.harry@yahoo.ca>
2017-08-15 09:50:08 -0400

    Push to production

--- index.defines.php
+++ index.defines.php
@@ -4,10 +4,11 @@
         "Jacky" => '<a class="collab-link" href="https://jackyliao123.tk">Jacky Liao</a>',
         "Henry" => '<a class="collab-link" href="https://guhenry3.tk">Henry Gu</a>',
         "Ben" => '<a class="collab-link" href="https://bcheng.cf">Benjamin Cheng</a>',
+        "Jim" => '<a class="collab-link" href="https://jimgao.tk">Jim Gao</a>',
         "Aaron" => 'Aaron Du',
         "Jaden" => 'Jaden Wang',
         "Andy" => 'Andy Huang',
-        "Jim" => '<a class="collab-link" href="https://jimgao.tk">Jim Gao</a>'
+        "Eric" => 'Eric Li'
     );
 
     $linksImgs = array(
