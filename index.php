76ed2b9afbf07d3e74bb7f1ab9e4c8d2a6bfebfd
Harry Zhang <zh.harry@yahoo.ca>
2017-08-15 07:49:44 -0400

    Testig

--- index.php
+++ index.php
@@ -148,6 +148,10 @@
                             <div class="learnMore-row" id="project-<?=$row["id"]?>">
                                 <div class="learnMore-left">
                                     <img class="learnMore-left-content" src="content/gallery/<?=$row["gallery"]?>">
+                                    <?php
+                                        $visit = json_decode($json, true);
+                                        var_dump($visit);
+                                    ?>
                                     <center><a href="http://ideashare.ml">View Site</a></center>
                                 </div>
                                 <div class="learnMore-right">
