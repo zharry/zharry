a06b425137aa8a943ea6f281b006ae3117dfca8b
Harry Zhang <zh.harry@yahoo.ca>
2017-08-28 23:26:47 -0400

    Prep for Project OpenAccess

---
+++ projects/openaccess/info.php
@@ -0,0 +1,12 @@
+<?php
+
+	require_once('/etc/mysql-creds/mysql-creds.php');
+	require_once('/etc/other-creds/creds.php');
+
+	echo $mysql-creds["host"];
+	echo $project_oa["host"];
+	echo $test;
+	
+	phpinfo();
+	
+?>
