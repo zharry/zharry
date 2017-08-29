0835515a179e51dcc774e75e81633a3fa0aac6db
Harry Zhang <zh.harry@yahoo.ca>
2017-08-29 08:10:29 -0400

    Finished Prep

---
+++ projects/openaccess/includes/connection.php
@@ -0,0 +1,8 @@
+<?php
+	require_once('/etc/mysql-creds/mysql-creds.php');
+	$conn = mysqli_connect($project_oa["host"], $project_oa["user"], $project_oa["pass"], $project_oa["data"]);
+
+    if (!$conn) {
+        die("Error establishing database connection, please contact Harry if you see this message!");
+    }
+?>
---
+++ projects/openaccess/includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title><?php if(isset($title)){ echo $title; } else { echo "Project: Open Access" } ?></title>
+    <link rel="stylesheet" href="style/main.css">
+</head>
+<body>
