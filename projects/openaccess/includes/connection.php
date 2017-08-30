b222984889d419f9673b2c86f6c5f477670c2fd4
Harry Zhang <zh.harry@yahoo.ca>
2017-08-30 17:10:56 -0400

    Fixed mysql connection var

--- projects/openaccess/includes/connection.php
+++ projects/openaccess/includes/connection.php
@@ -2,7 +2,7 @@
 	require_once('/etc/mysql-creds/mysql-creds.php');
 	//$conn = mysqli_connect("localhost", "root", "", "db");
 
-	$conn = mysqli_connect($projectoa["host"], $projectoa["user"], $projectoa["pass"], $projectoa["data"]);
+	$conn = mysqli_connect($project_oa["host"], $project_oa["user"], $project_oa["pass"], $project_oa["data"]);
 	
     if (!$conn) {
         die("Error establishing database connection, please contact Harry if you see this message!");
