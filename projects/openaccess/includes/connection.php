1761a09cdb29000396467d54c242b29fb4771039
Harry Zhang <zh.harry@yahoo.ca>
2017-08-30 16:55:39 -0400

    Initial Testing

--- projects/openaccess/includes/connection.php
+++ projects/openaccess/includes/connection.php
@@ -1,7 +1,9 @@
 <?php
-	// require_once('/etc/mysql-creds/mysql-creds.php');
-	$conn = mysqli_connect("localhost", "root", "", "openaccess");
+	require_once('/etc/mysql-creds/mysql-creds.php');
+	//$conn = mysqli_connect("localhost", "root", "", "db");
 
+	$conn = mysqli_connect($projectoa["host"], $projectoa["user"], $projectoa["pass"], $projectoa["data"]);
+	
     if (!$conn) {
         die("Error establishing database connection, please contact Harry if you see this message!");
     }
