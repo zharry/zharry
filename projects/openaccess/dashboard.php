68676af5d50c94dc226a26647d3e41cbcd46b7e4
Harry Zhang <zh.harry@yahoo.ca>
2017-08-30 22:36:32 -0400

    QOL

--- projects/openaccess/dashboard.php
+++ projects/openaccess/dashboard.php
@@ -84,10 +84,7 @@
 				// Create SE Internal Account
 				$createUserOutput = shell_exec("/etc/other-creds/vpncmd/vpncmd ".$project_oa_se["host"].":".$project_oa_se["port"]." /SERVER /HUB:".$project_oa_se["hub"]." /PASSWORD:".$project_oa_se["pass"]." /CMD UserCreate ".$seUsername." /GROUP:none /REALNAME:none /NOTE:none");
 				$changePassOutput = shell_exec("/etc/other-creds/vpncmd/vpncmd ".$project_oa_se["host"].":".$project_oa_se["port"]." /SERVER /HUB:".$project_oa_se["hub"]." /PASSWORD:".$project_oa_se["pass"]." /CMD UserPassWord ".$seUsername." /PASSWORD:".$sePassword);
-				
-				echo $createUserOutput;
-				echo $changePassOutput;
-				
+		
 				// Update DB about changes
 				$stmt = mysqli_prepare($conn, "UPDATE users SET softetheruser = ?, softetherpass = ? WHERE username = ?");
 				mysqli_stmt_bind_param($stmt, "sss", $seUsername , $sePassword, $username);
@@ -108,6 +105,11 @@
 			mysqli_stmt_execute($stmt);
 			if (mysqli_stmt_affected_rows($stmt) != 1)
 				$error[] = "Unknown error occured, please contact Harry.";
+			else {
+				$_SESSION["perms"] = $newPerm;
+				$_SESSION["softetherUser"] = $seUsername;
+				$_SESSION["softetherPass"] = $sePassword;
+			}
 		} else {
 			echo "Incorrect Code!<br/>";
 		}
@@ -162,6 +164,8 @@ if ($_SESSION["perms"] == "general" || $_SESSION["perms"] == "full" || $_SESSION
 <hr/>
 <h1>Softether Configuration</h1>
 <?php 
+	echo "Username: ".$_SESSION["softetherUser"]."<br/>";
+	echo "Password: ".$_SESSION["softetherPass"]."<br/>";
 }
 ?>
 
