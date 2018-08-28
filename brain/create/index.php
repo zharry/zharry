cf93439dffe84ec6782f16da10fce0a0e9667309
Harry Zhang <zh.harry@yahoo.ca>
2018-08-28 12:52:47 -0400

    Added Tutorial Type

--- brain/create/index.php
+++ brain/create/index.php
@@ -83,6 +83,7 @@
                         <option>Raw</option>
                         <option>Idea</option>
                         <option>Note</option>
+                        <option>Tutorial</option>
                         <option>Knowledge</option>
                         <option>URL</option>
                     </select>
