a5373a4c2a71b3b643821a85a976e0b4ed7d90fb
Harry Zhang <zh.harry@yahoo.ca>
2018-11-13 11:03:25 -0500

    Test

--- dev/manychat/api.php
+++ dev/manychat/api.php
@@ -1,19 +1,62 @@
 <?php
 
     $data = json_decode(file_get_contents('php://input'), true);
+        
+    $return = json_decode('{
+        "version": "v2",
+        "content": {
+            "messages": [{
+                "type": "text",
+                "text": "Auth Token invalid!"
+            }]
+        }
+    }', true);
+    $setUserInfo = json_decode('{
+        "version": "v2",
+        "actions": [
+            {
+                "action": "set_field_value",
+                "tag_name": "full_name",
+                "value": "John Smith"
+            },
+            {
+                "action": "set_field_value",
+                "tag_name": "email",
+                "value": "john.smith@gmail.com"
+            }
+        ]
+    }', true);
+    $setError = json_decode('{
+        "version": "v2",
+        "actions": [
+            {
+                "action": "set_field_value",
+                "tag_name": "error",
+                "value": "WRONG_ROOM"
+            }
+        ]
+    }', true);
     
-    
-    $return = json_decode('{"version": "v2","content": {"messages": [{"type": "text","text": "Auth Token invalid!"}]}}', true);
-    
-    if($data["authToken"] == "abc123") {
-        $req = $data["req"];
+    $req = $data["req"];
+    if($data["auth"] == "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hbnljaGF0IiwicGVybXMiOlsiQm90Il0sImNsaWVudF9pZCI6WyIwMDAwMDAwMDAwIl0sImlhdCI6MTU0MjA2ODg1MX0.7-6tsYVtbvYLJqTnhB4jUKTvJYEHKF2QZkoSIU7dBdo2pxwgIpi1zS9xkQ4kx9R6DBSSxFbgx_ylFdeQSAedKw") {
         if ($req == "weather")
-            $return["content"]["messages"][0]["text"] = "Waterloo, ON: 11/11/2018 - 3 Degrees, Feels Like 0. 70% POP, Light Snow";
+            $return["content"]["messages"][0]["text"] 
+                = "Waterloo, ON: 11/11/2018 - 
+                   3 Degrees, Feels Like 0, 
+                   70% POP, Light Snow";
         else if ($req == "phone")
-            $return["content"]["messages"][0]["text"] = "123-456-7890";
+            $return["content"]["messages"][0]["text"] 
+                = "123-456-7890";
         else if ($req == "address")
-            $return["content"]["messages"][0]["text"] = "123 Address Ave.";
+            $return["content"]["messages"][0]["text"] 
+                = "123 Address Ave.";
     }
-    echo json_encode($return);
+    if ($req != "userinfo")
+        echo json_encode($return);
+    else
+        if ($req["room"] == "Room-101" || $req["room"] == "101")
+            echo json_encode($setUserInfo);
+        else
+            echo json_encode($setError);
 
 ?>
