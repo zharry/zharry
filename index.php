b264c2bb43e4176d891cdf3270124b521aa6462e
Harry Zhang <zh.harry@outlook.com>
2019-09-19 17:14:59 -0400

    Revert "testing prod mysql"
    This reverts commit 8fe19036cf6ddc8b467666e14a75f3f0bfe98bac.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	else	
 		$conn = mysqli_connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASS'), "zharry");
 	if (!$conn) {
-		die("Error establishing database connection! " . "Using: " . getenv('MYSQL_HOST'));
+		die("Error establishing database connection!");
 	}
 	
 	// Get Collaborators Names
